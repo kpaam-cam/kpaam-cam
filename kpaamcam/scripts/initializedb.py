0c41a6254dd600fca34966ed368838145437c5ee
Robert Forkel <xrotwang@googlemail.com>
2022-12-02 10:46:28 +0100

    update publisher and deps

--- kpaamcam/scripts/initializedb.py
+++ kpaamcam/scripts/initializedb.py
@@ -27,9 +27,9 @@ def main(args):
         id=kpaamcam.__name__,
         domain='kpaamcam.clld.org',
 
-        publisher_name="Max Planck Institute for the Science of Human History",
-        publisher_place="Jena",
-        publisher_url="http://www.shh.mpg.de",
+        publisher_name="Max Planck Institute for Evolutionary Anthropology",
+        publisher_place="Leipzig",
+        publisher_url="http://www.eva.mpg.de",
         license="http://creativecommons.org/licenses/by/4.0/",
         jsondata={
             'license_icon': 'cc-by.png',
